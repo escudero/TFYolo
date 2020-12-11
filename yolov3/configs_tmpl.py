09485f3e2475610ec0bc0fa30c7cca0c1b59bd33
Escudero <claudioe@gmail.com>
2020-12-10 23:27:38 -0300

    Ajuste no template do arquivo de configuração

--- yolov3/configs_tmpl.py
+++ yolov3/configs_tmpl.py
@@ -6,7 +6,7 @@ YOLO_V4_WEIGHTS="model_data/yolov4.weights"
 YOLO_V3_TINY_WEIGHTS="model_data/yolov3-tiny.weights"
 YOLO_V4_TINY_WEIGHTS="model_data/yolov4-tiny.weights"
 YOLO_CUSTOM_WEIGHTS=False # "checkpoints/yolov3_custom" # used in evaluate_mAP.py and custom model detection, if not using leave False
-                            # YOLO_CUSTOM_WEIGHTS also used with TensorRT and custom model detection
+                          # YOLO_CUSTOM_WEIGHTS also used with TensorRT and custom model detection
 YOLO_COCO_CLASSES="model_data/coco.names"
 YOLO_STRIDES=[8, 16, 32]
 YOLO_IOU_LOSS_THRESH=0.5
@@ -35,7 +35,7 @@ TRAIN_WARMUP_EPOCHS=2
 TRAIN_EPOCHS=100
 
 # TEST options
-TEST_ANNOT_PATH= "mnist/mnist_test.txt"
+TEST_ANNOT_PATH="mnist/mnist_test.txt"
 TEST_BATCH_SIZE=4
 TEST_INPUT_SIZE=416
 TEST_DATA_AUG=False
