ee205306c536c2275bea714d0c0269484291eef8
Escudero <claudioe@gmail.com>
2020-12-16 23:32:55 -0300

    Adicionando a detecção de imagem como padrão

--- detection.py
+++ detection.py
@@ -23,12 +23,12 @@ video_path = os.path.join("IMAGES", "futebol.mp4")
 output_path = os.path.join("IMAGES", "predict")
 
 model = Create_Yolo(input_size=YOLO_INPUT_SIZE, class_names=YOLO_CLASSES)
-model.load_weights(f"./checkpoints/{TRAIN_MODEL_NAME}")
+model.load_weights(os.path.join(TRAIN_CHECKPOINTS_FOLDER, TRAIN_MODEL_NAME))
 
 output_path = None
 
 dt1 = datetime.now()
-# detect_image(model, image_path, output_path, input_size=YOLO_INPUT_SIZE, show=True, return_images=False, class_names=YOLO_CLASSES, rectangle_colors=(255,0,0))
-detect_video(model, video_path, output_path, input_size=YOLO_INPUT_SIZE, show=True, class_names=YOLO_CLASSES, chunksize=10, score_threshold=0.3, iou_threshold=0.45, rectangle_colors='')
+detect_image(model, image_path, output_path, input_size=YOLO_INPUT_SIZE, show=True, return_images=False, class_names=YOLO_CLASSES, rectangle_colors=(255,0,0))
+# detect_video(model, video_path, output_path, input_size=YOLO_INPUT_SIZE, show=True, class_names=YOLO_CLASSES, chunksize=10, score_threshold=0.3, iou_threshold=0.45, rectangle_colors='')
 dt2 = datetime.now()
-print(f'Tempo: {dt2 - dt1}')
+print(f'Processing time: {dt2 - dt1}')
